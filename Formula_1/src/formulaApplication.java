3b7bb8f4f3a47693fbb2ee404519e2880afdece1
MKuijpers <mika.kuijpers@live.nl>
2016-11-28 16:30:22 +0100

    Added application java file

--- Formula_1/src/formulaApplication.java
+++ Formula_1/src/formulaApplication.java
@@ -1,9 +1,57 @@
-public class formulaApplication {
+import java.awt.Button;
 
-	public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.geometry.VPos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
 
-		System.out.println("Test if there is sick shit yo!");
-		System.out.println("test");
+public class formulaApplication extends Application
+{
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
+	
+	public void start(Stage stage)
+	{
+		stage.setTitle("Formula 1 Manager");
 		
+		Group root = new Group();
+		Scene theScene = new Scene(root);
+		stage.setScene(theScene);
+		
+		Canvas canvas = new Canvas(1920*.90, 1080*.90);
+		root.getChildren().add(canvas);
+		
+		
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+		
+		gc.setFill(Color.GRAY);
+		gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+		
+		gc.setFill(Color.RED);
+		gc.setStroke(Color.BLACK);
+		gc.setLineWidth(2);
+		Font font = Font.font("Impact", FontWeight.BOLD, 120);
+		gc.setFont(font);
+		gc.setTextAlign(TextAlignment.CENTER);
+		gc.setTextBaseline(VPos.CENTER);
+		gc.fillText("FORMULA 1 MANAGER", canvas.getWidth()/2, canvas.getHeight()/2);
+		gc.strokeText("FORMULA 1 MANAGER", canvas.getWidth()/2, canvas.getHeight()/2);
+		
+		Button startNewGame = new Button("Start New Game");
+		Button resumeGame = new Button("Resume Game");
+		Button Highscores = new Button("Highscores");
+		Button exitGame = new Button("Exit Game");
+		
+		stage.show();
 	}
 }
