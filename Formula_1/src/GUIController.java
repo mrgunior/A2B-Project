0945942c0586511f1eb9fef4e87f9df9fac9821b
MKuijpers <mika.kuijpers@live.nl>
2016-12-02 16:27:15 +0100

    Added another screen for a new game. The button Start New Game will now bring you to the NewGame.fxml screen.

--- Formula_1/src/GUIController.java
+++ Formula_1/src/GUIController.java
@@ -1,42 +1,57 @@
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class GUIController implements Initializable
 {
 	@Override
 	public void initialize(URL location, ResourceBundle resources)
 	{
-				
+
 	}
-	
+
 	@FXML
-	private void startNewGame(ActionEvent event)
+	private void startNewGame(ActionEvent event) throws IOException
 	{
-		System.out.println("Start new game");
+		System.out.println("Starting new game...");
+		Parent root = FXMLLoader.load(this.getClass().getResource("NewGame.fxml"));
+		Scene scene = new Scene(root);
+		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		stage.setScene(scene);
+		stage.show();
 	}
+
 	@FXML
 	private void resume(ActionEvent event)
 	{
 		System.out.println("Resume");
 	}
+
 	@FXML
 	private void options(ActionEvent event)
 	{
 		System.out.println("Options");
 	}
+
 	@FXML
 	private void highscores(ActionEvent event)
 	{
 		System.out.println("Show highscores");
 	}
+
 	@FXML
 	private void exit(ActionEvent event)
 	{
 		System.out.println("Exit");
 	}
-	
+
 }
