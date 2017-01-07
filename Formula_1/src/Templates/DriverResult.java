4a1479556754868912aea98937286e2c24631d1a
MKuijpers <mika.kuijpers@live.nl>
2017-01-07 19:50:21 +0100

    Added tests for Results and DriverResult (100% coverage)

--- Formula_1/src/Templates/DriverResult.java
+++ Formula_1/src/Templates/DriverResult.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 import com.sun.xml.internal.bind.v2.runtime.RuntimeUtil.ToStringAdapter;
 
 import manager.model.Driver;
+import sun.reflect.generics.tree.VoidDescriptor;
 
 public class DriverResult
 {
@@ -24,6 +25,15 @@ public class DriverResult
 	{
 		return carId;
 	}
+	
+	/**
+	 * Setter for car ID
+	 * @param carId - Car ID to set for the DriverResult
+	 */
+	public void setCarId(int carId)
+	{
+		this.carId = carId;
+	}
 
 	/**
 	 * Getter for driver Name
@@ -34,6 +44,15 @@ public class DriverResult
 	{
 		return name;
 	}
+	
+	/**
+	 * Setter for name
+	 * @param name - Name to set for the DriverResult
+	 */
+	public void setName(String name)
+	{
+		this.name = name;
+	}
 
 	/**
 	 * Getter for result time
@@ -43,6 +62,15 @@ public class DriverResult
 	public double getTime()
 	{
 		return time;
+	}
+	
+	/**
+	 * Setter for carId
+	 * @param time - Time to set for the DriverResult
+	 */
+	public void setTime(double time)
+	{
+		this.time = time;
 	}
 
 	/**
@@ -57,6 +85,44 @@ public class DriverResult
 		this.carId = carId;
 		this.name = name;
 		this.time = time;
+	} 
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+		{
+			return true;
+		}
+		if (obj == null)
+		{
+			return false;
+		}
+		if (!(obj instanceof DriverResult))
+		{
+			return false;
+		}
+		DriverResult other = (DriverResult) obj;
+		if (carId != other.carId)
+		{
+			return false;
+		}
+		if (name == null)
+		{
+			if (other.name != null)
+			{
+				return false;
+			}
+		}
+		else if (!name.equals(other.name))
+		{
+			return false;
+		}
+		if (Double.doubleToLongBits(time) != Double.doubleToLongBits(other.time))
+		{
+			return false;
+		}
+		return true;
 	}
 
 	/**
---
+++ Formula_1/src/manager/test/ResultsTest.java
@@ -0,0 +1,99 @@
+package manager.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.sun.org.apache.xalan.internal.xsltc.compiler.util.CompareGenerator;
+
+import org.hamcrest.*;
+
+import Templates.DriverResult;
+import Templates.Results;
+
+public class ResultsTest
+{
+	private DriverResult	driverResult1;
+	private int				carId1	= 5;
+	private String			name1	= "Max Verstappen";
+	private double			time1	= 12500;
+
+	private DriverResult	driverResult2;
+	private int				carId2	= 3;
+	private String			name2	= "Max Verstappen";
+	private double			time2	= 15000;
+
+	Results results;
+
+	@Before
+	public void setUp() throws Exception
+	{
+		driverResult1 = new DriverResult(carId1, name1, time1);
+		driverResult2 = new DriverResult(carId2, name2, time2);
+		results = new Results();
+	}
+
+	@Test
+	public void testResults()
+	{
+		assertTrue(results instanceof Results);
+	}
+
+	@Test
+	public void testAddResult()
+	{
+		results.addResult(driverResult1);
+		assertEquals(1, results.getResults().size());
+	}
+
+	@Test
+	public void testGetResult()
+	{
+		results.addResult(driverResult1);
+		assertEquals(driverResult1, results.getResult(0));
+	}
+
+	@Test
+	public void testGetResultsSize()
+	{
+		results.addResult(driverResult1);
+		assertEquals(1, results.getResults().size());
+		results.addResult(driverResult2);
+		assertEquals(2, results.getResults().size());
+	}
+
+	@Test
+	public void testGetResults()
+	{
+		results.addResult(driverResult1);
+		assertEquals(driverResult1, results.getResults().get(0));
+	}
+
+	@Test
+	public void testSortResultsByTime()
+	{
+		results.addResult(driverResult1);
+		results.addResult(driverResult2);
+		results.sortResultsByTime();
+		assertTrue(Double.compare(results.getResult(0).getTime(), results.getResult(1).getTime()) < 0);
+	}
+
+	@Test
+	public void testSortResultsByCarId()
+	{
+		results.addResult(driverResult1);
+		results.addResult(driverResult2);
+		results.sortResultsByCarId();
+		assertTrue(results.getResult(0).getCarId() < results.getResult(1).getCarId());
+	}
+
+	@Test
+	public void testToString()
+	{
+		results.addResult(driverResult1);
+		results.addResult(driverResult2);
+		assertEquals("[Max Verstappen (5) : 12500.0, Max Verstappen (3) : 15000.0]", results.toString());
+	}
+
+}
